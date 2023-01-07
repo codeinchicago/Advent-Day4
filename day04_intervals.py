04c07422ee5175297dc9f2e97dbb50692c98a953
Vance Cole <104577414+codeinchicago@users.noreply.github.com>
2023-01-07 13:03:25 -0600

    Add files via upload

---
+++ day04_intervals.py
@@ -0,0 +1,20 @@
+
+count = 0
+with open("2022/data/day04_input.txt") as f:
+    lines = f.readlines()
+    newstuff = []
+    for line in lines:
+        line = line.strip()
+        b = line.replace(","," ")
+        a = b.replace("-",",")
+        pair1, pair2 = a.split(" ")
+        lower1, higher1 = [int(value) for value in pair1.split(",")]
+        lower2, higher2 = [int(value) for value in pair2.split(",")]
+        if lower1 >= lower2 and higher1 <= higher2:
+            count += 1
+            print(lower1,higher1,lower2,higher2)
+        elif lower2 >= lower1 and higher2 <= higher1:
+            count += 1
+            print(lower1,higher1,lower2,higher2)
+        print("Count is:")
+        print(count)
